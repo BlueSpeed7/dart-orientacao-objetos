702e5483ae7565cbda2b840d2949cc15df4b88fa
Fernando <bluespeed8@gmail.com>
2023-07-15 10:59:19 -0300

    Herança no Dart

--- bin/dart_orientacao_objetos.dart
+++ bin/dart_orientacao_objetos.dart
@@ -1,68 +1,88 @@
 void main() {
-  Fruta fruta01 = Fruta('laranja', 'amarela', 'amarga', 20, 30, 56.4);
-  Fruta fruta02 = Fruta.nomeados(
-      nome: 'uva',
-      cor: 'roxa',
-      sabor: 'doce',
-      diasDesdeColheita: 30,
-      diasParaMadura: 20,
-      peso: 40.7,
-      isMadura: true);
-  print(fruta01.frase());
-  print(fruta02.frase());
+  Fruta fruta01 = Fruta('laranja', 'amarela', 30.5, 'amarga', 20);
+  Citrica citrica01 = Citrica('limão', 'verde', 20.0, 'ácido', 15, 7.5);
+  Legume legume01 = Legume('pimentão', 'vermelho', 42.3, 25, true);
+  fruta01.estaMadura(30);
+  citrica01.estaMadura(10);
+  print(legume01.cozinhar());
+  print(legume01.cozinhar());
+  print(fruta01.toString());
+  print(citrica01.toString());
+  print(legume01.toString());
 }
 
-class Fruta {
-  String nome, cor, sabor;
-  int diasDesdeColheita, diasParaMadura;
-  double peso;
+class Fruta extends Alimento {
+  String sabor;
+  int diasDesdeColheita;
   bool? isMadura;
 
   Fruta(
-    this.nome,
-    this.cor,
-    this.sabor,
-    this.diasDesdeColheita,
-    this.diasParaMadura,
-    this.peso,
-  );
+      String nome, String cor, double peso, this.sabor, this.diasDesdeColheita)
+      : super(nome, cor, peso);
 
-  Fruta.nomeados(
-      {required this.nome,
-      required this.cor,
-      required this.sabor,
-      required this.diasDesdeColheita,
-      required this.diasParaMadura,
-      required this.peso,
-      required this.isMadura});
-
-  bool? estaMadura() {
+  bool? estaMadura(int diasParaMadura) {
     isMadura ??= diasDesdeColheita >= diasParaMadura;
     return isMadura;
   }
 
-  int funcQuantosDiasMadura(int dias) {
-    int quantosDiasFaltam = diasParaMadura - dias;
-    return quantosDiasFaltam;
-  }
-
-  String frase() {
+  @override
+  String toString() {
     String result = '';
-    isMadura = estaMadura();
-    int quantosDiasFaltam = funcQuantosDiasMadura(diasDesdeColheita);
     if (isMadura == false) {
-      result = 'A $nome de cor $cor com sabor $sabor pesa $peso gramas.\n'
-          'Ela foi colhida há $diasDesdeColheita dias '
-          'e precisa de $diasParaMadura dias para amadurecer, '
-          'logo, ela não está madura.\n'
-          'Faltam $quantosDiasFaltam dias para a $nome ficar madura.';
+      result = 'O(a) $nome de cor $cor com sabor $sabor pesa $peso gramas.\n'
+          'Ele(a) foi colhido(a) há $diasDesdeColheita dias e não está maduro(a).';
     } else {
-      result = 'A $nome de cor $cor com sabor $sabor pesa $peso gramas.\n'
-          'Ela foi colhida há $diasDesdeColheita dias '
-          'e precisa de $diasParaMadura dias para amadurecer, '
-          'logo, ela está madura.';
+      result = 'O(a) $nome de cor $cor com sabor $sabor pesa $peso gramas.\n'
+          'Ele(a) foi colhido(a) há $diasDesdeColheita dias e está maduro(a).';
     }
-
     return result;
   }
+}
+
+class Alimento {
+  String nome, cor;
+  double peso;
+
+  Alimento(this.nome, this.cor, this.peso);
+
+  @override
+  String toString() {
+    return 'Este(a) $nome pesa $peso gramas e é $cor.';
+  }
+}
+
+class Legume extends Alimento {
+  int diasDesdeColheita;
+  bool? isPrecisaCozinhar;
+
+  Legume(String nome, String cor, double peso, this.diasDesdeColheita,
+      this.isPrecisaCozinhar)
+      : super(nome, cor, peso);
+
+  String cozinhar() {
+    String result = '';
+    if (isPrecisaCozinhar == true) {
+      result = 'Pronto, o(a) $nome está cozinhando.';
+      isPrecisaCozinhar = false;
+    } else {
+      result = 'Nem precisa cozinhar.';
+    }
+    return result;
+  }
+}
+
+class Citrica extends Fruta {
+  double nivelAzedo;
+
+  Citrica(String nome, String cor, double peso, String sabor,
+      int diasDesdeColheita, this.nivelAzedo)
+      : super(nome, cor, peso, sabor, diasDesdeColheita);
+}
+
+class Noz extends Fruta {
+  double porcentagemOleoNatural;
+
+  Noz(String nome, String cor, double peso, String sabor, int diasDesdeColheita,
+      this.porcentagemOleoNatural)
+      : super(nome, cor, peso, sabor, diasDesdeColheita);
 }
